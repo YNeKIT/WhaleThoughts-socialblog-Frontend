4810056bdcb7db3a6b0a2da1de49b84fa602f8dc
YNeKIT <nikitascotigo@gmail.com>
2024-06-24 22:51:48 +0300

    try to deploy work

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios  from 'axios';
 
 
 const instance = axios.create({
-    baseURL :  process.env.REACT_APP_API_URL,
+    baseURL :  process.env.REACT_APP_API_URL   || 
+    "https://whale-thoughts-socialblog-frontend.vercel.app" ,
 });
 
 instance.interceptors.request.use((config)  => {
